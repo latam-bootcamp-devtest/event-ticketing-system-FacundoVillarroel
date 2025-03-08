ac24a18e3020e48cdae1dcea548a7b491e34e17f
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-03-08 15:18:39 -0300

    [feature] - pagination in booking history

--- backend/src/controller/usersController.js
+++ backend/src/controller/usersController.js
@@ -3,12 +3,36 @@ const Ticket = require("../model/ticketModel");
 const getBookingHistory = async (req, res, next) => {
   try {
     const userId = req.params.userId;
-    const { page, Pagesize, startDate, endDate, sort, search } = req.query;
+    const { page, pageSize, startDate, endDate, sort, search } = req.query;
     const bookings = await Ticket.find({ userId });
+
     if (!bookings) {
       res.status(404).send({ message: "this User do not have any booking" });
       return;
     }
+
+    const eventsIds = bookings.map((ticket) => ticket.eventId);
+
+    //pagination
+    let pageCount = 1;
+    if (pageSize) {
+      pageCount = Math.ceil(bookings.length / pageSize);
+    }
+
+    if (page && pageSize) {
+      if (page > pageCount) {
+        page = pageCount;
+      }
+
+      const paginationBookingList = bookings.slice(
+        page * pageSize - pageSize,
+        page * pageSize
+      );
+
+      res.send(paginationBookingList);
+      return;
+    }
+    
     res.send(bookings);
   } catch (error) {
     console.error("Error getting booking history: ", error);
