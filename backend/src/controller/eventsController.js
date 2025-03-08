25f7207320196bd8c67e0a10a16c896ec03c0ded
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-03-08 10:58:45 -0300

    [validation] - create events validates date is in future

--- backend/src/controller/eventsController.js
+++ backend/src/controller/eventsController.js
@@ -1,9 +1,33 @@
 const Event = require("../model/eventModel");
 
+
+const getEvents = async () => {
+  try {
+    const { page, pageSize } = req.query;
+    const events = await Event.find();
+    res.send(events);
+  } catch (error) {
+    console.error("Error getting events: ", error);
+    res.status(500).send({ message: "Error getting events" });
+  }
+};
+
 const createEvent = async (req, res, next) => {
   try {
     const { name, date, availableSeats } = req.body;
-    const event = { name, date, availableSeats };
+    const dateObj = new Date(date);
+    const today = new Date();
+    if (dateObj < today) {
+      res.status(400).send({ message: "Date must be in the future" });
+      return;
+    }
+    const dateString = dateObj.toISOString();
+
+    if (availableSeats <= 0) {
+      res.status(400).send({ message: "availableSeats must be bigger than 0" });
+      return;
+    }
+    const event = { name, date: dateString, availableSeats };
     const eventCreated = await new Event(event).save();
     res.status(201).send(eventCreated);
   } catch (error) {
